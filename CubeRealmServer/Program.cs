532efbfd7d9662fde6400f697fcacffad76b678b
Zirolek <vmail5249@gmail.com>
2024-01-27 13:05:46 +0500

    add testing plugins path

--- CubeRealmServer/Program.cs
+++ CubeRealmServer/Program.cs
@@ -9,14 +9,21 @@ class Program
 {
     public static void Main(string[] args)
     {
+        bool isDebug = args.Contains("--debug");
+        List<Type> plugins = new();
+        Action<IServiceCollection, Type> pluginsLoader = (collection, type) =>
+        {
+            collection.AddSingleton(type);
+            plugins.Add(type);
+        };
         ServiceProvider serviceProvider = new ServiceCollection()
             .AddLogging(builder => builder.AddSimpleConsole(options =>
             {
                 options.ColorBehavior = LoggerColorBehavior.Enabled;
                 options.IncludeScopes = true;
                 options.TimestampFormat = "HH:mm:ss.fff ";
-            }).SetMinimumLevel(LogLevel.Trace))
-            .AddFromDirectory(Path.Combine(".", "plugins"), (collection, type) => collection.AddSingleton(type))
+            }).SetMinimumLevel(isDebug ? LogLevel.Trace : LogLevel.Information))
+            .AddFromDirectory(Path.Combine(isDebug ? "../../../../CubeRealmTesting/" : ".", "plugins"), pluginsLoader)
             .BuildServiceProvider();
     }
 }
