089cf5a15f31ccc680eb92168d04237f0ee36dbd
Zirolek <vmail5249@gmail.com>
2024-01-27 13:37:20 +0500

    Update DI and Config load

--- CubeRealmServer/Program.cs
+++ CubeRealmServer/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Configuration;
+using ConfigurationAPI;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using PluginAPI;
@@ -7,23 +9,39 @@ namespace CubeRealmServer;
 
 class Program
 {
-    public static void Main(string[] args)
+    public static void Main(List<string> args)
     {
-        bool isDebug = args.Contains("--debug");
+        int debugIndex = args.IndexOf("--debug");
+        string workDirectory = ".";
+
+        if (debugIndex != -1)
+            workDirectory = args[debugIndex + 1];
+        
+        string pluginsPath = Path.Combine(workDirectory, "plugins");
+        string configPath = Path.Combine(workDirectory, "configs");
+        
         List<Type> plugins = new();
-        Action<IServiceCollection, Type> pluginsLoader = (collection, type) =>
+
+        void PluginsLoader(IServiceCollection collection, Type type)
         {
             collection.AddSingleton(type);
             plugins.Add(type);
-        };
+        }
+
         ServiceProvider serviceProvider = new ServiceCollection()
             .AddLogging(builder => builder.AddSimpleConsole(options =>
             {
                 options.ColorBehavior = LoggerColorBehavior.Enabled;
                 options.IncludeScopes = true;
                 options.TimestampFormat = "HH:mm:ss.fff ";
-            }).SetMinimumLevel(isDebug ? LogLevel.Trace : LogLevel.Information))
-            .AddFromDirectory(Path.Combine(isDebug ? "../../../../CubeRealmTesting/" : ".", "plugins"), pluginsLoader)
+            }).SetMinimumLevel(debugIndex != -1 ? LogLevel.Trace : LogLevel.Information))
+            //Configuration
+            .AddTransient<IConfigLoader, ConfigLoader>()
+            .AddFromDirectory(pluginsPath, PluginsLoader)
             .BuildServiceProvider();
+
+        ConfigLoader configLoader = (ConfigLoader)serviceProvider.GetRequiredService<IConfigLoader>();
+        configLoader.SaveDefaults(configPath);
+        configLoader.Load(configPath);
     }
 }
