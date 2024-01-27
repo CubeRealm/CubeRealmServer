a43c7740db130d6d8f1d826f790b5b04fc970f69
s1stemdev <108710611+s1stemdev@users.noreply.github.com>
2024-01-27 12:38:45 +0500

    ConfigLoader.cs

--- Configuration/ConfigLoader.cs
+++ Configuration/ConfigLoader.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using ConfigurationAPI;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -8,6 +9,11 @@ public class ConfigLoader : IConfigLoader
 {
 
     private ILogger<ConfigLoader> Logger { get; }
+
+    private Dictionary<string, List<string>> Resources => new Dictionary<string, List<string>>()
+    {
+        { "settings", ["mobs_spawn.json", "server.json", "world.json"] }
+    };
 
     public ConfigLoader(ILogger<ConfigLoader> logger)
     {
@@ -28,7 +34,55 @@ public class ConfigLoader : IConfigLoader
             return default;
         }
     }
-    
-    public void Save(object obj) {}
+
+    public void Save(object obj, string path)
+    {
+        path = Path.Combine(Directory.GetCurrentDirectory(), path);
+
+        string output = JsonConvert.SerializeObject(obj);
+        if(File.Exists(path)) return;
+
+        try
+        {
+            File.Create(path);
+            File.WriteAllText(path, output);
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e.Message);
+            throw;
+        }
+    }
+
+    public void SaveDefaults()
+    {
+        const string allResources = "Configuration.Resources.";
+        
+        foreach (var items in Resources)
+        {
+            string assemblyPath = $"{allResources}content.base.{items.Key}.";
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), items.Key);
+
+            if (!Directory.Exists(filePath))
+                Directory.CreateDirectory(filePath);
+            
+            foreach (var fileName in items.Value)
+                SaveResource($"{assemblyPath}{fileName}", Path.Combine(filePath, fileName));
+        }
+    }
+
+    private void SaveResource(string assemblyFile, string file, bool overrideFile = false)
+    {
+        if (File.Exists(file))
+            return;
+        Logger.LogTrace("assemblyFile {} file {}", assemblyFile, file);
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        Stream? stream = assembly.GetManifestResourceStream(assemblyFile);
+        if (stream == null)
+            throw new NullReferenceException("Resource not found");
+        using (var fileStream = new FileStream(file, FileMode.CreateNew))
+            for (int i = 0; i < stream.Length; i++)
+                fileStream.WriteByte((byte)stream.ReadByte());
+    }
     
 }
