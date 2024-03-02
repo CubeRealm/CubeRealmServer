54683ab9ba6a9640e896fdbfea42a7a3fdeb73cf
s1stemdev <108710611+s1stemdev@users.noreply.github.com>
2024-03-02 22:12:42 +0500

    IWorld.cs docs

--- World.API/World/IWorld.cs
+++ World.API/World/IWorld.cs
@@ -5,17 +5,51 @@ using World.API.Item;
 
 namespace World.API.World;
 
+/// <summary>
+/// Interface of the game world
+/// </summary>
 public interface IWorld
 {
     
+    /// <summary>
+    /// 2D array of chunks (x and z)
+    /// </summary>
     IChunk[,] Chunks { get; set; }
+    
+    /// <summary>
+    /// Name of the world
+    /// </summary>
     string WorldName { get; set; }
+    
+    /// <summary>
+    /// Generator for world
+    /// </summary>
     IWorldGenerator Generator { get; set; }
 
+    /// <summary>
+    /// Generates chunk in specific location
+    /// </summary>
+    /// <param name="location">Location of the chunk</param>
     void GenerateChunk(ChunkLocation location);
     
+    /// <summary>
+    /// Placing block in location
+    /// </summary>
+    /// <param name="block"></param>
+    /// <param name="location"></param>
     void SetBlock(IBlock block, Location location);
+    
+    /// <summary>
+    /// Placing block (item) in location
+    /// </summary>
+    /// <param name="block"></param>
+    /// <param name="location"></param>
     void SetBlock(IItem block, Location location);
+    
+    /// <summary>
+    /// Removing (replacing by AIR) in location
+    /// </summary>
+    /// <param name="location"></param>
     void RemoveBlock(Location location);
 
 }
