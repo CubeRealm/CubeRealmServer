94f197ba3adeb86d988ede9501d34cef2e539037
s1stemdev <108710611+s1stemdev@users.noreply.github.com>
2024-03-02 20:50:43 +0500

    Test docs

--- Plugin.API/IPlugin.cs
+++ Plugin.API/IPlugin.cs
@@ -1,12 +1,34 @@
 namespace PluginAPI;
 
+/// <summary>
+///  Entry point of your plugin. You should to implement this
+///  If you want to use Dependencies you need to get Main Class of plugin you wanted to use
+/// </summary>
 public interface IPlugin
 {
     
+    /// <summary>
+    /// Name of plugin which you will see in console
+    /// </summary>
     string Name { get; set; }
+    
+    /// <summary>
+    /// Version of your plugin
+    /// </summary>
     string Version { get; set; }
+    
+    /// <summary>
+    /// Authors of the plugin
+    /// </summary>
     string[] Authors { get; set; }
     
+    /// <summary>
+    /// Method Load executing after adding plugin in list
+    /// </summary>
     void Load();
+    
+    /// <summary>
+    /// Method Enable executing after all plugins has been added
+    /// </summary>
     void Enable();
 }
